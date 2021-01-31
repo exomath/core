53b252cf790a8bd8244e54f2fdfa1ffb175bc011
Jake McKown <jbmckown@gmail.com>
2021-01-31 12:14:14 -0700

    Add type guard

--- src/assert.ts
+++ src/assert.ts
@@ -1,7 +1,9 @@
-export function assert(assertion: boolean, message: string | (() => string), messenger?: string) {
+export function assert(assertion: boolean, message: string | (() => string), messenger?: string): assertion is true {
   messenger = messenger ? messenger + ': ' : '';
 
   if (!assertion) {
     throw new Error(`${messenger}${typeof message === 'string' ? message : message()}`);
   }
+
+  return true;
 }
