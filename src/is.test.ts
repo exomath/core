78aaf404c04d4460613cb6430542829a8bb0587e
usahmdh <uzairsyedahmed@gmail.com>
2021-06-29 23:19:07 -0500

    Add unit tests
    Add tests for :
    
    isBoolean
    isInfinity
    isInteger
    isIntegerArray
    isNan
    isNull

--- src/is.test.ts
+++ src/is.test.ts
@@ -7,13 +7,13 @@ import {
 
 import {
   isArray,
-  /* isBoolean,
+  isBoolean,
   isInfinity,
   isInteger,
   isIntegerArray,
   isNaN,
   isNull,
-  isObject,
+  /* isObject,
   isNumber,
   isNumberArray,
   isString,
@@ -33,6 +33,7 @@ import {
   isTypedArray*/
 } from './is';
 
+// isArray(value)
 describe('isArray(value)', () => {
   it('Should be a function', () => {
     expect(isArray).to.be.a('function');
@@ -44,5 +45,99 @@ describe('isArray(value)', () => {
 
   it('Should should return false when "value" is not type any[]', () => {
     expect(isArray(1)).to.equal(false);
+  });
+});
+
+// isBoolean(value)
+describe('isBoolean(value)', () => {
+  it('Should be a function', () => {
+    expect(isBoolean).to.be.a('function');
+  });
+
+  it('Should return true when "value" is type boolean', () => {
+    expect(isBoolean(true)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not type boolean', () => {
+    expect(isBoolean('This is a string')).to.equal(false);
+  });
+});
+
+// isInfinity(value)
+describe('isInfinity(value)', () => {
+  it('Should be a function', () => {
+    expect(isInfinity).to.be.a('function');
+  });
+
+  it('Should return true when "value" is Infinity', () => {
+    expect(isInfinity(Infinity)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not Infinity', () => {
+    expect(isInfinity('This is a string')).to.equal(false);
+  });
+});
+
+// isInteger(value)
+describe('isInteger(value)', () => {
+  it('Should be a function', () => {
+    expect(isInteger).to.be.a('function');
+  });
+
+  it('Should return true when "value" is an Integer', () => {
+    expect(isInteger(24)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not an Integer', () => {
+    expect(isInteger(24.245)).to.equal(false);
+  });
+});
+
+// isIntegerArray(value)
+describe('isInteger(value)', () => {
+  it('Should be a function', () => {
+    expect(isIntegerArray).to.be.a('function');
+  });
+
+  it('Should return true when "value" is type number[] and each array entry is an integer', () => {
+    expect(isIntegerArray([1, 2, 3])).to.equal(true);
+  });
+
+  it('Should return false when "value" is not type number[] or a number[] where each array entry is not an integer', () => {
+    expect(isIntegerArray([1, '2', true])).to.equal(false);
+  });
+
+  it('Should return false when "value" is not type number[] or a number[] where each array entry is not an integer', () => {
+    expect(isIntegerArray([1, 2.2, 3.3])).to.equal(false);
+  });
+});
+
+// isNaN(value)
+describe('isNaN(value)', () => {
+  it('Should be a function', () => {
+    expect(isNaN).to.be.a('function');
+  });
+
+  it('Should return true when "value" is NaN', () => {
+    expect(isNaN(NaN)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not NaN', () => {
+    expect(isNaN(24.245)).to.equal(false);
+  });
+});
+
+// isNull(value)
+describe('isNull(value)', () => {
+  it('Should be a function', () => {
+    expect(isNull).to.be.a('function');
+  });
+
+  it('Should return true when "value" is null', () => {
+    expect(isNull(null)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not null', () => {
+    expect(isNull(24.245)).to.equal(false);
   });
 });
