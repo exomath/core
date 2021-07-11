8a2283a909654e48113459f16edfdb2c26440e3f
usahmdh <uzairsyedahmed@gmail.com>
2021-07-11 15:12:53 -0500

    Add more test, Make type guard optional
    Add more unit tests. Add till isInt8Array.
    Make typeguard for all `is` functions optional

--- src/is.test.ts
+++ src/is.test.ts
@@ -9,17 +9,17 @@ import {
   isArray,
   isBoolean,
   isInfinity,
+  isInt8Array,
   isInteger,
   isIntegerArray,
   isNaN,
   isNull,
-  /* isObject,
   isNumber,
   isNumberArray,
+  isObject,
   isString,
   isStringArray,
-  isInt8Array,
-  isUint8Array,
+  /* isUint8Array,
   isUint8ClampedArray,
   isInt16Array,
   isUint16Array,
@@ -39,12 +39,22 @@ describe('isArray(value)', () => {
     expect(isArray).to.be.a('function');
   });
 
-  it('Should should return true when "value" is type any[]', () => {
-    expect(isArray([1, '2', true])).to.equal(true);
+  it('Should return true when "value" is type any[]', () => {
+    expect(isArray([])).to.equal(true);
+    expect(isArray([1, 2, 3])).to.equal(true);
+    expect(isArray(['a', 'b', 'c'])).to.equal(true);
+    expect(isArray([1, '2', true, {}])).to.equal(true);
   });
 
-  it('Should should return false when "value" is not type any[]', () => {
-    expect(isArray(1)).to.equal(false);
+  it('Should return false when "value" is not type any[]', () => {
+    expect(isArray('This is a string')).to.equal(false);
+    expect(isArray(100)).to.equal(false);
+    expect(isArray(undefined)).to.equal(false);
+    expect(isArray(null)).to.equal(false);
+    expect(isArray(true)).to.equal(false);
+    expect(isArray(false)).to.equal(false);
+    expect(isArray({})).to.equal(false);
+    expect(isArray()).to.equal(false);
   });
 });
 
@@ -62,8 +72,11 @@ describe('isBoolean(value)', () => {
   it('Should return false when "value" is not type boolean', () => {
     expect(isBoolean('This is a string')).to.equal(false);
     expect(isBoolean(100)).to.equal(false);
+    expect(isBoolean(undefined)).to.equal(false);
+    expect(isBoolean(null)).to.equal(false);
     expect(isBoolean([])).to.equal(false);
     expect(isBoolean({})).to.equal(false);
+    expect(isBoolean()).to.equal(false);
   });
 });
 
@@ -79,6 +92,14 @@ describe('isInfinity(value)', () => {
 
   it('Should return false when "value" is not Infinity', () => {
     expect(isInfinity('This is a string')).to.equal(false);
+    expect(isInfinity(100)).to.equal(false);
+    expect(isInfinity(undefined)).to.equal(false);
+    expect(isInfinity(null)).to.equal(false);
+    expect(isInfinity(true)).to.equal(false);
+    expect(isInfinity(false)).to.equal(false);
+    expect(isInfinity([])).to.equal(false);
+    expect(isInfinity({})).to.equal(false);
+    expect(isInfinity()).to.equal(false);
   });
 });
 
@@ -89,11 +110,21 @@ describe('isInteger(value)', () => {
   });
 
   it('Should return true when "value" is an Integer', () => {
-    expect(isInteger(24)).to.equal(true);
+    expect(isInteger(0)).to.equal(true);
+    expect(isInteger(100)).to.equal(true);
+    expect(isInteger(100.00)).to.equal(true);
   });
 
   it('Should return false when "value" is not an Integer', () => {
-    expect(isInteger(24.245)).to.equal(false);
+    expect(isInteger('This is a String')).to.equal(false);
+    expect(isInteger(100.11)).to.equal(false);
+    expect(isInteger(undefined)).to.equal(false);
+    expect(isInteger(null)).to.equal(false);
+    expect(isInteger(true)).to.equal(false);
+    expect(isInteger(false)).to.equal(false);
+    expect(isInteger([])).to.equal(false);
+    expect(isInteger({})).to.equal(false);
+    expect(isInteger()).to.equal(false);
   });
 });
 
@@ -104,15 +135,28 @@ describe('isIntegerArray(value)', () => {
   });
 
   it('Should return true when "value" is type number[] and each array entry is an integer', () => {
+    expect(isIntegerArray([0, 0, 0])).to.equal(true);
     expect(isIntegerArray([1, 2, 3])).to.equal(true);
+    expect(isIntegerArray([0.00, 1.00, 2.00, 3.00])).to.equal(true);
+    expect(isIntegerArray([])).to.equal(true);
   });
 
   it('Should return false when "value" is not type number[]', () => {
-    expect(isIntegerArray([1, '2', true])).to.equal(false);
+    expect(isIntegerArray('This is a String')).to.equal(false);
+    expect(isIntegerArray(100)).to.equal(false);
+    expect(isIntegerArray(undefined)).to.equal(false);
+    expect(isIntegerArray(null)).to.equal(false);
+    expect(isIntegerArray(true)).to.equal(false);
+    expect(isIntegerArray(false)).to.equal(false);
+    expect(isIntegerArray(['String', 2, false])).to.equal(false);
+    expect(isIntegerArray({})).to.equal(false);
+    expect(isIntegerArray()).to.equal(false);
   });
 
   it('Should return false when "value" is type number[] but not every array entry is an integer', () => {
+    expect(isIntegerArray([1.1, 2.2, 3.3])).to.equal(false);
     expect(isIntegerArray([1, 2.2, 3.3])).to.equal(false);
+    expect(isIntegerArray([1, 2, 3.3])).to.equal(false);
   });
 });
 
@@ -127,7 +171,15 @@ describe('isNaN(value)', () => {
   });
 
   it('Should return false when "value" is not NaN', () => {
-    expect(isNaN(24.245)).to.equal(false);
+    expect(isNaN('This is a String')).to.equal(false);
+    expect(isNaN(100)).to.equal(false);
+    expect(isNaN(undefined)).to.equal(false);
+    expect(isNaN(null)).to.equal(false);
+    expect(isNaN(true)).to.equal(false);
+    expect(isNaN(false)).to.equal(false);
+    expect(isNaN([])).to.equal(false);
+    expect(isNaN({})).to.equal(false);
+    expect(isNaN()).to.equal(false);
   });
 });
 
@@ -142,6 +194,158 @@ describe('isNull(value)', () => {
   });
 
   it('Should return false when "value" is not null', () => {
-    expect(isNull(24.245)).to.equal(false);
+    expect(isNull('This is a String')).to.equal(false);
+    expect(isNull(100)).to.equal(false);
+    expect(isNull(undefined)).to.equal(false);
+    expect(isNull(true)).to.equal(false);
+    expect(isNull(false)).to.equal(false);
+    expect(isNull([])).to.equal(false);
+    expect(isNull({})).to.equal(false);
+    expect(isNull()).to.equal(false);
+  });
+});
+
+// isObject(value)
+describe('isObject(value)', () => {
+  it('Should be a function', () => {
+    expect(isObject).to.be.a('function');
+  });
+
+  it('Should return true when "value" is type any{}', () => {
+    expect(isObject({})).to.equal(true);
+    expect(isObject({a:1, b:2, c:3})).to.equal(true);
+    expect(isObject({a:'String', b:true, c:[], d:{}})).to.equal(true);
+    expect(isObject([])).to.equal(true);
+    expect(isObject(null)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not type any{}', () => {
+    expect(isObject('This is a string')).to.equal(false);
+    expect(isObject(100)).to.equal(false);
+    expect(isObject(undefined)).to.equal(false);
+    expect(isObject(true)).to.equal(false);
+    expect(isObject(false)).to.equal(false);
+    expect(isObject()).to.equal(false);
+  });
+});
+
+// isNumber(value)
+describe('isNumber(value)', () => {
+  it('Should be a function', () => {
+    expect(isNumber).to.be.a('function');
+  });
+
+  it('Should return true when "value" is a Number', () => {
+    expect(isNumber(100)).to.equal(true);
+    expect(isNumber(100.25)).to.equal(true);
+    expect(isNumber(0)).to.equal(true);
+  });
+
+  it('Should return false when "value" is not a Number', () => {
+    expect(isNumber('This is a String')).to.equal(false);
+    expect(isNumber(undefined)).to.equal(false);
+    expect(isNumber(null)).to.equal(false);
+    expect(isNumber(true)).to.equal(false);
+    expect(isNumber(false)).to.equal(false);
+    expect(isNumber([])).to.equal(false);
+    expect(isNumber({})).to.equal(false);
+    expect(isNumber()).to.equal(false);
+  });
+});
+
+// isNumberArray(value)
+describe('isNumberArray(value)', () => {
+  it('Should be a function', () => {
+    expect(isNumberArray).to.be.a('function');
+  });
+
+  it('Should return true when "value" is type number[]', () => {
+    expect(isNumberArray([1, 2, 3])).to.equal(true);
+    expect(isNumberArray([0.11, 1.11, 2.22, 3.33])).to.equal(true);
+    expect(isNumberArray([])).to.equal(true);
+  });
+
+  it('Should return false when "value" is not type number[]', () => {
+    expect(isNumberArray('This is a String')).to.equal(false);
+    expect(isNumberArray(100)).to.equal(false);
+    expect(isNumberArray(undefined)).to.equal(false);
+    expect(isNumberArray(null)).to.equal(false);
+    expect(isNumberArray(true)).to.equal(false);
+    expect(isNumberArray(false)).to.equal(false);
+    expect(isNumberArray(['String', 2, false])).to.equal(false);
+    expect(isNumberArray([1, 2, 'String', true])).to.equal(false);
+    expect(isNumberArray({})).to.equal(false);
+    expect(isNumberArray()).to.equal(false);
+  });
+});
+
+// isString(value)
+describe('isString(value)', () => {
+  it('Should be a function', () => {
+    expect(isString).to.be.a('function');
+  });
+
+  it('Should return true when "value" is a String', () => {
+    expect(isString('This is a String')).to.equal(true);
+  });
+
+  it('Should return false when "value" is not a String', () => {
+    expect(isString(100)).to.equal(false);
+    expect(isString(undefined)).to.equal(false);
+    expect(isString(null)).to.equal(false);
+    expect(isString(true)).to.equal(false);
+    expect(isString(false)).to.equal(false);
+    expect(isString([])).to.equal(false);
+    expect(isString({})).to.equal(false);
+    expect(isString()).to.equal(false);
+  });
+});
+
+// isStringArray(value)
+describe('isStringArray(value)', () => {
+  it('Should be a function', () => {
+    expect(isStringArray).to.be.a('function');
+  });
+
+  it('Should return true when "value" is type String[]', () => {
+    expect(isStringArray(['String1', 'String2', 'String3'])).to.equal(true);
+    expect(isStringArray(['1', '2', '3'])).to.equal(true);
+    expect(isStringArray([])).to.equal(true);
+  });
+
+  it('Should return false when "value" is not type String[]', () => {
+    expect(isStringArray('This is a String')).to.equal(false);
+    expect(isStringArray(100)).to.equal(false);
+    expect(isStringArray(undefined)).to.equal(false);
+    expect(isStringArray(null)).to.equal(false);
+    expect(isStringArray(true)).to.equal(false);
+    expect(isStringArray(false)).to.equal(false);
+    expect(isStringArray(['String', 2, false])).to.equal(false);
+    expect(isStringArray({})).to.equal(false);
+    expect(isStringArray()).to.equal(false);
+  });
+});
+
+// isInt8Array(value)
+describe('isInt8Array(value)', () => {
+  it('Should be a function', () => {
+    expect(isInt8Array).to.be.a('function');
+  });
+
+  it('Should return true when "value" is an Int8Array', () => {
+    expect(isInt8Array(new Int8Array())).to.equal(true);
+  });
+
+  it('Should return false when "value" is not an Int8Array', () => {
+    expect(isInt8Array('This is a String')).to.equal(false);
+    expect(isInt8Array(100)).to.equal(false);
+    expect(isInt8Array(undefined)).to.equal(false);
+    expect(isInt8Array(null)).to.equal(false);
+    expect(isInt8Array(true)).to.equal(false);
+    expect(isInt8Array(false)).to.equal(false);
+    expect(isInt8Array([])).to.equal(false);
+    expect(isInt8Array({})).to.equal(false);
+    expect(isInt8Array(new Float64Array())).to.equal(false);
+    expect(isInt8Array()).to.equal(false);
   });
 });
--- src/is.ts
+++ src/is.ts
@@ -1,94 +1,94 @@
-export function isArray(value: any): value is any[] {
+export function isArray(value?: any): value is any[] {
   return Array.isArray(value);
 }
 
-export function isBoolean(value: any): value is boolean {
+export function isBoolean(value?: any): value is boolean {
   return typeof value === 'boolean' || value instanceof Boolean;
 }
 
-export function isInfinity(value: any): value is number {
+export function isInfinity(value?: any): value is number {
   return isNumber(value) && !isNaN(value) && !isFinite(value);
 }
 
-export function isInteger(value: any): value is number {
+export function isInteger(value?: any): value is number {
   return isNumber(value) && Number.isInteger(value);
 }
 
-export function isIntegerArray(value: any): value is number[] {
+export function isIntegerArray(value?: any): value is number[] {
   return isArray(value) && value.every((item: any) => isInteger(item));
 }
 
-export function isNaN(value: any): value is number {
+export function isNaN(value?: any): value is number {
   return isNumber(value) && Number.isNaN(value);
 }
 
-export function isNull(value: any): value is null {
+export function isNull(value?: any): value is null {
   return value === null;
 }
 
-export function isObject(value: any): value is object {
+export function isObject(value?: any): value is object {
   return typeof value === 'object';
 }
 
-export function isNumber(value: any): value is number {
+export function isNumber(value?: any): value is number {
   return typeof value === 'number' || value instanceof Number;
 }
 
-export function isNumberArray(value: any): value is number[] {
+export function isNumberArray(value?: any): value is number[] {
   return isArray(value) && value.every((item: any) => isNumber(item));
 }
 
-export function isString(value: any): value is string {
+export function isString(value?: any): value is string {
   return typeof value === 'string' || value instanceof String;
 }
 
-export function isStringArray(value: any): value is string[] {
+export function isStringArray(value?: any): value is string[] {
   return isArray(value) && value.every((item: any) => isString(item));
 }
 
 // TypedArrays
 
-export function isInt8Array(value: any): value is Int8Array {
+export function isInt8Array(value?: any): value is Int8Array {
   return value instanceof Int8Array;
 }
 
-export function isUint8Array(value: any): value is Uint8Array {
+export function isUint8Array(value?: any): value is Uint8Array {
   return value instanceof Uint8Array;
 }
 
-export function isUint8ClampedArray(value: any): value is Uint8ClampedArray {
+export function isUint8ClampedArray(value?: any): value is Uint8ClampedArray {
   return value instanceof Uint8ClampedArray;
 }
 
-export function isInt16Array(value: any): value is Int16Array {
+export function isInt16Array(value?: any): value is Int16Array {
   return value instanceof Int16Array;
 }
 
-export function isUint16Array(value: any): value is Uint16Array {
+export function isUint16Array(value?: any): value is Uint16Array {
   return value instanceof Uint16Array;
 }
 
-export function isInt32Array(value: any): value is Int32Array {
+export function isInt32Array(value?: any): value is Int32Array {
   return value instanceof Int32Array;
 }
 
-export function isUint32Array(value: any): value is Uint32Array {
+export function isUint32Array(value?: any): value is Uint32Array {
   return value instanceof Uint32Array;
 }
 
 // Not currently supporting BigInt64Array and BigUint64Array
 
-export function isFloat32Array(value: any): value is Float32Array {
+export function isFloat32Array(value?: any): value is Float32Array {
   return value instanceof Float32Array;
 }
 
-export function isFloat64Array(value: any): value is Float64Array {
+export function isFloat64Array(value?: any): value is Float64Array {
   return value instanceof Float64Array;
 }
 
 export type IntArray = Int8Array | Int16Array | Int32Array;
 
-export function isIntArray(value: any): value is IntArray {
+export function isIntArray(value?: any): value is IntArray {
   return isInt8Array(value)
       || isInt16Array(value)
       || isInt32Array(value);
@@ -96,7 +96,7 @@ export function isIntArray(value: any): value is IntArray {
 
 export type UintArray = Uint8Array | Uint16Array | Uint32Array;
 
-export function isUintArray(value: any): value is UintArray {
+export function isUintArray(value?: any): value is UintArray {
   return isUint8Array(value)
       || isUint16Array(value)
       || isUint32Array(value);
@@ -104,14 +104,14 @@ export function isUintArray(value: any): value is UintArray {
 
 export type FloatArray = Float32Array | Float64Array;
 
-export function isFloatArray(value: any): value is FloatArray {
+export function isFloatArray(value?: any): value is FloatArray {
   return isFloat32Array(value)
       || isFloat64Array(value);
 }
 
 export type TypedArray = IntArray | UintArray | FloatArray;
 
-export function isTypedArray(value: any): value is TypedArray {
+export function isTypedArray(value?: any): value is TypedArray {
   return isIntArray(value)
       || isUintArray(value)
       || isFloatArray(value);
